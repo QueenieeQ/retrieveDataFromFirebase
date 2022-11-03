3ec8b68d4d014cddc8569539321f80cbb141a4fc
QueenieeQ <xuanquyhnvn@gmail.com>
2022-11-03 19:04:35 +0700

    Update date of birth picker

--- app/src/main/java/com/example/relatimedatabase/UpdateData.java
+++ app/src/main/java/com/example/relatimedatabase/UpdateData.java
@@ -3,11 +3,14 @@ package com.example.relatimedatabase;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,6 +22,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
@@ -28,6 +32,7 @@ public class UpdateData extends AppCompatActivity {
     DatabaseReference databaseReference;
     private Button delete;
     private Spinner spinner2;
+    private EditText dateofbirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +41,46 @@ public class UpdateData extends AppCompatActivity {
         setContentView(binding.getRoot());
 //        setContentView(R.layout.activity_update_data);
 
-
 //        delete =findViewById(R.id.deleteBtn);
+//        dateofbirth.findViewById(R.id.dateofbirth);
 //        logout =findViewById(R)
+        dateofbirth= binding.dateofbirth.findViewById(R.id.dateofbirth);
+        binding.dateofbirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                final Calendar c = Calendar.getInstance();
+                int year =c.get(Calendar.YEAR);
+                int month= c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        UpdateData.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // on below line we are setting date to our edit text.
+                                dateofbirth.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
+
+
+
+            }
+        });
 
         binding.updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
                 Spinner spinner = (Spinner)findViewById(R.id.spinner2);
                 String gender = spinner.getSelectedItem().toString();
                 String userName = binding.userName.getText().toString();
@@ -58,90 +92,32 @@ public class UpdateData extends AppCompatActivity {
 //                String lastName = binding.lastname.getText().toString();
 //                String age = binding.age.getText().toString();
 
-
                 updateData(userName, dateOfBirth, email, gender, occupation, userNumber);
 
 
-            }
-        });
-//        delete.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent =new Intent(UpdateData.this, DeleteData.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-
-
-
     }
-//
-//    private void addListenerOnSpinnerItemSelection() {
-//
-//        spinner2 = (Spinner) findViewById(R.id.spinner2);
-//        spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-//    }
-//
-//    private void addListenerOnButton() {
-//        void addListenerOnButton() {
-//
-////            spinner1 = (Spinner) findViewById(R.id.spinner1);
-//            spinner2 = (Spinner) findViewById(R.id.spinner2);
-////            btnSubmit = (Button) findViewById(R.id.btnSubmit);
-//
-////            btnSubmit.setOnClickListener(new OnClickListener() {
-//
-//                @Override
-//                public void onClick(View v) {
-//
-//                    Toast.makeText(MyAndroidAppActivity.this,
-//                            "OnClickListener : " +
-//                                    "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
-//                                    "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
-//                            Toast.LENGTH_SHORT).show();
-//                }
-//
-//            });
-//        }
-//
-//    }
-//
-//    private void addItemsOnSpinner2() {
-//
-//        spinner2 = (Spinner) findViewById(R.id.spinner2);
-//        List<String> list = new ArrayList<String>();
-//        list.add("Male");
-//        list.add("Female");
-//        list.add("Other");
-//        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_spinner_item, list);
-//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner2.setAdapter(dataAdapter);
-//    }
 
     private void updateData(String dateOfBirth,String email,String gender,String occupation,String userName,String userNumber) {
 
         HashMap User = new HashMap();
-        User.put(("userNumber"),userNumber);
-        User.put(("userName"),userName);
-        User.put("Dateofbirth",dateOfBirth);
-        User.put("Email",email);
-        User.put("Gender",gender);
-        User.put("Occupation",occupation);
+        User.put(("userNumber"), userNumber);
+        User.put(("userName"), userName);
+        User.put("Dateofbirth", dateOfBirth);
+        User.put("Email", email);
+        User.put("Gender", gender);
+        User.put("Occupation", occupation);
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
         databaseReference.child(userNumber).updateChildren(User).addOnCompleteListener(new OnCompleteListener() {
             @Override
             public void onComplete(@NonNull Task task) {
 
-                if (task.isSuccessful()){
-                    if(userName.isEmpty()){
+                if (task.isSuccessful()) {
+                    if (userName.isEmpty()) {
                         binding.userName.setError("Enter valid username");
-                    }else if(userName.length()<3){
-                        binding.userName.setError("Enter vailed username");
-                    }else{
+                    } else if (userName.length() < 3) {
+                        binding.userName.setError(" username too short");
+                    } else {
                         Toast.makeText(UpdateData.this, "Something went wrong", Toast.LENGTH_SHORT).show();
                         binding.userName.setText("");
                     }
@@ -155,18 +131,12 @@ public class UpdateData extends AppCompatActivity {
                     binding.occupation.setText("");
 
 
-                    Toast.makeText(UpdateData.this,"Successfully Updated",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(UpdateData.this, "Successfully Updated", Toast.LENGTH_SHORT).show();
 
-                }else {
+                } else {
 
-                    Toast.makeText(UpdateData.this,"Failed to Update",Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(UpdateData.this, "Failed to Update", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
-
-    }
-
-
-}
+    }});}}
